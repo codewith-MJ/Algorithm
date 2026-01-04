da6ad66601be52a0f6bbbd40c3cb07e128ad1bb0
codewith.mj / Minji Kim <mj.kim.coding@gmail.com>
2026-01-04 13:16:53 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,15 +1,18 @@
-const solution = (numer1, denom1, numer2, denom2) => {
-    const denom = denom1 * denom2;
-    const numer = numer1 * denom2 + numer2 * denom1;
-    
-    let a = denom;
-    let b = numer;
-    
+const getGcd = (a, b) => {
     while (b !== 0) {
         const temp = a % b;
         a = b;
         b = temp;
     }
+    
+    return a;
+};
+
+const solution = (numer1, denom1, numer2, denom2) => {
+    const denom = denom1 * denom2;
+    const numer = numer1 * denom2 + numer2 * denom1;
+    
+    const gcd = getGcd(numer, denom);
         
-    return [numer / a, denom / a];
+    return [numer / gcd, denom / gcd];
 };
