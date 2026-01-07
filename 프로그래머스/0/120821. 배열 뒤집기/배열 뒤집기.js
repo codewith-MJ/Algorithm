5b43d7ecd3b93d7146136f5917a4bce16cf88bd8
codewith.mj / Minji Kim <mj.kim.coding@gmail.com>
2026-01-07 15:23:31 +0900

    [level 0] Title: 배열 뒤집기, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
+++ 프로그래머스/0/120821. 배열 뒤집기/배열 뒤집기.js
@@ -1,14 +1,3 @@
 function solution(num_list) {
-    let left = 0;
-    let right = num_list.length - 1;
-    
-    while (left < right) {
-        const temp = num_list[left];
-        num_list[left] = num_list[right];
-        num_list[right] = temp;
-        left++;
-        right--;
-    }
-    
-    return num_list;
+    return [...num_list].reverse();
 }
